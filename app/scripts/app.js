aff1568be8a8ad1dc34f620b410e80fdddf8d405
Marcos Castany <marcos.castany@southworks.net>
2013-09-30 23:59:38 -0300

    First Commit

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('hubApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
