918a7c51928e8cb1fe6a0eefc5c63d211fcd340a
Marcos Castany <marcos.castany@southworks.net>
2013-10-08 09:07:39 -0300

    Spike

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,40 +1,57 @@
 'use strict';
 
-var listOfEntries = [];
+//var listOfEntries = [];
 
-for (var j = 0; j < 5; j++) {
-    var subelements = [];
-    for (var i = 0; i < 5; i++) {
-        var subelements2 = [];
-        for (var k = 0; k < 5; k++) {
-            subelements2.push({
-                name: "Sub-Sub-Element" + k,
-                elements: []
-            });
-        }
-        subelements.push({
-            name: "Sub-Element" + i,
-            elements: subelements2
-        });
-    }
+//for (var j = 0; j < 5; j++) {
+//    var subelements = [];
+//    for (var i = 0; i < 5; i++) {
+//        var subelements2 = [];
+//        for (var k = 0; k < 5; k++) {
+//            subelements2.push({
+//                name: "Sub-Sub-Element" + k,
+//                elements: []
+//            });
+//        }
+//        subelements.push({
+//            name: "Sub-Element" + i,
+//            elements: subelements2
+//        });
+//    }
 
-    listOfEntries.push({
-        name: "Element" + j,
-        elements: subelements
-    });
-}
+//    listOfEntries.push({
+//        name: "Element" + j,
+//        elements: subelements
+//    });
+//}
 
-var node = {
-    name: "Master",
-    elements: listOfEntries
-};
+//var node = {
+//    name: "Master",
+//    elements: listOfEntries
+//};
 
 angular.module('hubApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-    $scope.menu = node;
+   
+      //$scope.menu = node;
+      var open = false;
+      document.querySelector('button').addEventListener("click", function () {
+          var valMenu = '',
+              valContainer = '';
+          if (!open) {
+              valContainer = 'translate(300px,0)';
+              //valMenu = 'translate(-100%,0)';
+          } else {
+              valMenu = 'translate(0,0)';
+              //valContainer = 'translate(0,0)';
+          }
+          
+          //document.querySelector('.menu').style.WebkitTransform = valMenu;
+          //document.querySelector('.menu').style.MozTransform = valMenu;
+          //document.querySelector('.menu').style.transform = valMenu;
+          
+          document.querySelector('.sub-container').style.WebkitTransform = valContainer;
+          document.querySelector('.sub-container').style.MozTransform = valContainer;
+          document.querySelector('.sub-container').style.transform = valContainer;
+          open = !open;
+      });
   });
--- app/scripts/directives/menu.js
+++ app/scripts/directives/menu.js
@@ -13,24 +13,41 @@ angular.module('hubApp')
           link: function postLink(scope, element, attrs) {
               var template =
                   '<div  class="mp-level">' +
-                    '<h2 class="icon icon-display">' +
+                      '<h2 class="icon icon-display">' +
                       scope.menulist.name +
-                    '</h2>' +
-                    '<a class="mp-back" href="#">back</a>';
-              
+                      '</h2>' +
+                      '<a class="mp-back" href="#">back</a>';
+
               if (angular.isArray(scope.menulist.elements)) {
                   template +=
                       '<ul>' +
-                          '<li class="icon icon-arrow-left" ng-repeat="val in menulist.elements">{{val.name}}' +
-                            '<menu menulist="val" parent="menulist" ></menu>' +
+                          '<li class="icon icon-arrow-left" ng-repeat="val in menulist.elements">' +
+                          '<a href="#">{{val.name}}</a>' +
+                          '<menu menulist="val" parent="menulist" ></menu>' +
                           '</li>' +
-                      '</ul>';
+                          '</ul>';
               }
-
               template += '</div>';
+
               var newElement = angular.element(template);
               $compile(newElement)(scope);
               element.replaceWith(newElement);
+
+              elem.find('li').forEach(function(el, i) {
+                  var subLevel = el.querySelector('div.mp-level');
+                  if (subLevel) {
+                      el.querySelector('a').addEventListener("click", function (ev) {
+                          ev.preventDefault();
+                          alert("Clicked an element");
+                      });
+                  }
+              });
+              
+              elem.find('.mp-back').addEventListener("click", function (ev) {
+                  ev.preventDefault();
+                  alert("Clicked back button");
+              });
+              
           }
       };
   });
