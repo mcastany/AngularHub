42691b19890f0e9467542e16e5430d6dc8d2ef2b
unknown <mcastany@I-NB115.SW.AD>
2013-10-08 21:29:56 -0300

    Continued with Spike

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -33,25 +33,38 @@ angular.module('hubApp')
   .controller('MainCtrl', function ($scope) {
    
       //$scope.menu = node;
-      var open = false;
-      document.querySelector('button').addEventListener("click", function () {
-          var valMenu = '',
-              valContainer = '';
+      var open = false,
+          openSubnivel = false,
+          initialWith = 300,
+          subWith = 40;
+
+      document.querySelector('button.opener').addEventListener("click", function () {
+          var valContainer = '';
+
           if (!open) {
-              valContainer = 'translate(300px,0)';
-              //valMenu = 'translate(-100%,0)';
+              valContainer = 'translate(' + initialWith + 'px,0)';
           } else {
-              valMenu = 'translate(0,0)';
-              //valContainer = 'translate(0,0)';
+              valContainer = 'translate(0,0)';
           }
-          
-          //document.querySelector('.menu').style.WebkitTransform = valMenu;
-          //document.querySelector('.menu').style.MozTransform = valMenu;
-          //document.querySelector('.menu').style.transform = valMenu;
           
           document.querySelector('.sub-container').style.WebkitTransform = valContainer;
           document.querySelector('.sub-container').style.MozTransform = valContainer;
           document.querySelector('.sub-container').style.transform = valContainer;
           open = !open;
+      });
+
+      document.querySelector('button.submenu-button').addEventListener("click", function () {
+          var valContainer = '';
+
+          if (!openSubnivel) {
+              valContainer = 'translate(' + (initialWith + subWith) + 'px,0)';
+          } else {
+              valContainer = 'translate(' + initialWith + 'px,0)';
+          }
+
+          document.querySelector('.sub-container').style.WebkitTransform = valContainer;
+          document.querySelector('.sub-container').style.MozTransform = valContainer;
+          document.querySelector('.sub-container').style.transform = valContainer;
+          openSubnivel = !openSubnivel;
       });
   });
