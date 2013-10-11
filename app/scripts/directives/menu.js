feb0383fb7811a2db446bf921b3305328f0e80f9
Marcos Castany <marcos.castany@southworks.net>
2013-10-11 19:15:14 -0300

    Improved spike

--- app/scripts/directives/menu.js
+++ app/scripts/directives/menu.js
@@ -15,31 +15,33 @@ angular.module('hubApp').directive('menu', function ($compile) {
         controller: function ($scope) {
             var open = false,
                 initialValue = 300,
-                indent = 40;
+                indent = 40,
+                container = $('.container'),
+                body = $('body');
+
+            var bodyFunction = function (e) {
+                e.preventDefault();
+                e.stopPropagation();
+                open = false;
+                updateStyles(container, 0);
+                body.off('click', bodyFunction);
+            };
+
 
             if ($scope.trigger) {
                 $($scope.trigger).on('click', function (ev) {
                     ev.preventDefault();
                     ev.stopPropagation();
-
-                    var container = $('.container'),
-                        valContainer = initialValue,
-                        bodyFunction = function (e) {
-                            ev.preventDefault();
-                            ev.stopPropagation();
-                            open = false;
-                            updateStyles(container, 0);
-                            $('body').off('click', bodyFunction);
-                        };
-
+                    var valContainer = initialValue;
                     if (open) {
                         valContainer = 0;
                     }
 
-                    $('body').on('click', bodyFunction);
+                    $('nav > .menu-level').addClass('menu-overlaped')
 
+                    body.on('click', bodyFunction);
                     updateStyles(container, valContainer);
-                    updateStyles($('.menu-level[data-level=1]'), 0);
+                    updateStyles($('.menu-level[data-level=0]'), 0);
                     open = !open;
                 });
             }
@@ -54,55 +56,58 @@ angular.module('hubApp').directive('menu', function ($compile) {
 
             $scope.open = function (event, name, level) {
                 var divE = $(event.currentTarget).parent().find('.' + name),
-                    container = $('.container'),
                     valContainer = initialValue + level * indent;
 
                 event.preventDefault();
                 event.stopPropagation();
 
                 updateStyles(container, valContainer);
-
-                updateStyles(divE, 0);
+                divE.addClass('menu-overlaped');
+                divE.css("-webkit-transform", '');
+                divE.css("-moz-transform", '');
+                divE.css("-ms-transform", '');
+                divE.css("-o-transform", '');
+                divE.css("transform", '');
 
                 //We need to move the elements that are hidden more to the left
                 $('.menu-level').each(function (index, item) {
-                    if ($(item).data('level') >= level && !item.classList.contains(name)) {
+                    if (
+                        $(item).data('level') > level ||
+                        ($(item).data('level') == level && !item.classList.contains(name)) ||
+                        ($(item).data('level') < level && !item.classList.contains("menu-overlaped"))
+                        ) {
                         //We need to check the level and if the same value
                         $(item).css("-webkit-transform", 'translate(-100%, 0) translate(-' + indent * level + 'px, 0)');
                         $(item).css("-moz-transform", 'translate(-100%, 0) translate(-' + indent * level + 'px, 0)');
                         $(item).css("-ms-transform", 'translate(-100%, 0) translate(-' + indent * level + 'px, 0)');
                         $(item).css("-o-transform", 'translate(-100%, 0) translate(-' + indent * level + 'px, 0)');
                         $(item).css("transform", 'translate(-100%, 0) translate(-' + indent * level + 'px, 0)');
-                    }
+                    } 
                 });
             };
 
             $scope.back = function (event, level) {
-                var container = $('.container'),
-                    valContainer = initialValue + level * indent;
+                var divE = $(event.currentTarget).parent(),
+                    valContainer = initialValue + (level - 1) * indent;
 
                 event.preventDefault();
                 event.stopPropagation();
 
-                updateStyles(container, valContainer);
+                divE.removeClass('menu-overlaped');
 
-                //We need to move the elements that are hidden more to the right
-                $('.menu-level').each(function (index, item) {
-                    if ($(item).data('level') >= level) {
-                        //We need to check the level and if the same value
-                        $(item).css("-webkit-transform", 'translate(-100%, 0) translate(-' + indent * level + 'px, 0)');
-                        $(item).css("-moz-transform", 'translate(-100%, 0) translate(-' + indent * level + 'px, 0)');
-                        $(item).css("-ms-transform", 'translate(-100%, 0) translate(-' + indent * level + 'px, 0)');
-                        $(item).css("-o-transform", 'translate(-100%, 0) translate(-' + indent * level + 'px, 0)');
-                        $(item).css("transform", 'translate(-100%, 0) translate(-' + indent * level + 'px, 0)');
-                    }
-                });
+                if (level == 0) {
+                    updateStyles(container, 0);
+                    body.off('click', bodyFunction);
+                    open = false;
+                } else {
+                    updateStyles(container, valContainer);
+                }
             };
 
 
         },
         link: function (scope, element, attrs) {
-            var level = scope.level || 1,
+            var level = scope.level || 0,
                 template = '<div  class="menu-level {{identifier}}" data-level="' + level + '">' +
                     '<h2 class="icon icon-display">' +
                       scope.menulist.name +
@@ -110,7 +115,7 @@ angular.module('hubApp').directive('menu', function ($compile) {
 
             if (angular.isArray(scope.menulist.elements)) {
                 level++;
-                template += '<a class="mp-back" ng-click="back($event, ' + (level - 1) + ')">Back</a>' +
+                template += '<a class="mp-back" ng-click="back($event, ' + (level - 1) + ', identifier)">Back</a>' +
                         '<ul>' +
                             '<li class="icon icon-arrow-left" ng-repeat="val in menulist.elements">' +
                             '<a ng-click="open($event, val.name, ' + level + ')">{{val.name}}</a>' +
