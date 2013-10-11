6fc0a4dd0a289b665fff8a2f33c81cc5450fcf2e
Marcos Castany <marcos.castany@southworks.net>
2013-10-11 09:25:05 -0300

    Pushed not working spike. To fix

--- app/scripts/directives/menu.js
+++ app/scripts/directives/menu.js
@@ -5,34 +5,78 @@ angular.module('hubApp')
   .directive('menu', function ($compile) {
       return {
           restrict: 'E',
+          replace: true,
           terminal: true,
           scope: {
               menulist: '=',
-              identifier: '='
+              identifier: '=',
+              level: "="
           },
           controller: function ($scope) {
-              $scope.open = function (e, v) {
-                  var elem = angular.element(e.srcElement);
-                  alert("Clicked an element");
+              var open = false,
+                  initialValue = 300,
+                  indent = 40;
+
+              var updateStyles = function (container, val) {
+                  container.css("-webkit-transform", 'translate(' + val + ',0)');
+                  container.css("-moz-transform", 'translate(' + val + ',0)');
+                  container.css("-ms-transform", 'translate(' + val + ',0)');
+                  container.css("-o-transform", 'translate(' + val + ',0)');
+                  container.css("transform", 'translate(' + val + ',0)');
               };
 
-              $scope.back = function (e, v) {
-                  var elem = angular.element(e.srcElement);
-                  alert("Clicked back button");
+              $scope.open = function (event, name, level) {
+                  var divE = $(event.currentTarget).parent().find('.' + name),
+                      container = $('.container'),
+                      valContainer = initialValue + level * indent;
+                  
+                  updateStyles(container, valContainer);
+                  
+                  //We need to move the elements that are hidden more to the left
+                  $('.container:not(.menu-level-open').forEach(function (item) {
+                      //We need to check the level and if the same value
+                      $(item).css("-webkit-transform", 'translate(-100%, 0) translate(' + indent * level + 'px, 0)');
+                      $(item).css("-moz-transform", 'translate(-100%, 0) translate(' + indent * level + 'px, 0)');
+                      $(item).css("-ms-transform", 'translate(-100%, 0) translate(' + indent * level + 'px, 0)');
+                      $(item).css("-o-transform", 'translate(-100%, 0) translate(' + indent * level + 'px, 0)');
+                      $(item).css("transform", 'translate(-100%, 0) translate(' + indent * level + 'px, 0)');
+                  });
               };
+
+              $scope.back = function (event, level) {
+                  var divE = $(event.currentTarget).parent().find('.' + name),
+                      container = $('.container'),
+                      valContainer = initialValue + level * indent;
+
+                  updateStyles(container, valContainer);
+                  
+                  //We need to move the elements that are hidden more to the right
+                  $('.container:not(.menu-level-open').forEach(function (item) {
+                      //We need to check the level and if the same value
+                      $(item).css("-webkit-transform", 'translate(-100%, 0) translate(' + indent * level + 'px, 0)');
+                      $(item).css("-moz-transform", 'translate(-100%, 0) translate(' + indent * level + 'px, 0)');
+                      $(item).css("-ms-transform", 'translate(-100%, 0) translate(' + indent * level + 'px, 0)');
+                      $(item).css("-o-transform", 'translate(-100%, 0) translate(' + indent * level + 'px, 0)');
+                      $(item).css("transform", 'translate(-100%, 0) translate(' + indent * level + 'px, 0)');
+                  });
+              };
+
+
           },
           link: function (scope, element, attrs) {
-              var template = '<div  class="menu-container {{identifier}}">' +
+              var level = scope.level || 1,
+                  template = '<div  class="menu-level {{identifier}}" data-level="' + level + '">' +
                       '<h2 class="icon icon-display">' +
                         scope.menulist.name +
                       '</h2>';
 
               if (angular.isArray(scope.menulist.elements)) {
-                  template += '<a class="mp-back" ng-click="back($event)">Back</a>' +
+                  level++;
+                  template += '<a class="mp-back" ng-click="back($event, ' + (level -1) + ')">Back</a>' +
                           '<ul>' +
                               '<li class="icon icon-arrow-left" ng-repeat="val in menulist.elements">' +
-                              '<a ng-click="open($event, val.name)">{{val.name}}</a>' +
-                                 '<menu menulist="val" identifier="val.name"></menu>' +
+                              '<a ng-click="open($event, val.name, ' + level + ')">{{val.name}}</a>' +
+                                 '<menu menulist="val" identifier="val.name" level="' + level + '"></menu>' +
                               '</li>' +
                           '</ul>';
               }
